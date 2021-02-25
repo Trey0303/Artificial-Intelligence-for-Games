7cbe9f95f07dc481893bf5022a36956e2c389eee
Trey Gleason <treygleason8@gmail.com>
2021-02-25 13:04:03 -0800

    Djikstra Update
    working on Build Phase

---
+++ Assets/Scenes/Djikstra/Scripts/DjikstraAlgorithm.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DjikstraAlgorithm : MonoBehaviour
+{
+    //Build Phase
+    public int gridWidth;
+    public int gridHeight;
+
+    public GameObject prefab;
+
+    public Tile[] tiles;
+
+    private void Start()
+    {
+        tiles = new Tile[gridWidth * gridHeight];
+
+        Vector3 offset = Vector3.zero;
+
+        for(int i = 0; i < gridHeight; ++i)
+        {
+            for (int j = 0; j < gridWidth; ++j)
+            {
+                GameObject newTile = Instantiate(prefab, transform.position + offset, transform.rotation);
+                tiles[i * gridWidth + j] = newTile.GetComponent<Tile>();
+                offset.x += 1.0f;
+            }
+
+            offset.x = 0.0f;
+            offset.z += 1.0f;
+        }
+    }
+
+    //Siege Phase
+
+}
---
+++ Assets/Scenes/Djikstra/Scripts/Tile.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Tile previousTile;
+    public int gScore;//cost
+}
