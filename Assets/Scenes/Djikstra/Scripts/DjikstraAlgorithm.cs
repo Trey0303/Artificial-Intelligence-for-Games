bc827b0f17aaadc2ace56ba6ccf5cde0d690fb4d
Trey Gleason <treygleason8@gmail.com>
2021-02-25 14:55:38 -0800

    Base Phase Update

--- Assets/Scenes/Djikstra/Scripts/DjikstraAlgorithm.cs
+++ Assets/Scenes/Djikstra/Scripts/DjikstraAlgorithm.cs
@@ -18,7 +18,8 @@ public class DjikstraAlgorithm : MonoBehaviour
 
         Vector3 offset = Vector3.zero;
 
-        for(int i = 0; i < gridHeight; ++i)
+        // spawn the tiles
+        for (int i = 0; i < gridHeight; ++i)
         {
             for (int j = 0; j < gridWidth; ++j)
             {
@@ -30,8 +31,84 @@ public class DjikstraAlgorithm : MonoBehaviour
             offset.x = 0.0f;
             offset.z += 1.0f;
         }
+
+        // setup connections
+        for (int i = 0; i < tiles.Length; ++i)
+        {
+            List<Tile> connectedTiles = new List<Tile>();
+
+            // if we're not at the left-edge
+            if (i % gridWidth != 0)
+            {
+                connectedTiles.Add(tiles[i - 1]);
+            }
+
+            // if we're not at the right-edge
+            if ((i + 1) % gridWidth != 0)
+            {
+                connectedTiles.Add(tiles[i + 1]);
+            }
+
+            // if we're not at the upper-edge
+            if (i < gridWidth * gridHeight - gridWidth)
+            {
+                connectedTiles.Add(tiles[i + gridWidth]);
+            }
+
+            // if we're not at the bottom-edge
+            if (i > gridWidth)
+            {
+                connectedTiles.Add(tiles[i - gridWidth]);
+            }
+
+            //tiles[i].connectedTiles = connectedTiles.ToArray();
+        }
+    }
+
+    private Tile GetCheapestTile(Tile[] arr)
+    {
+        float bestGScore = float.MaxValue;
+        Tile bestTile = null;
+
+        for (int i = 0; i < tiles.Length; ++i)
+        {
+            if (arr[i].gScore < bestGScore)
+            {
+                bestTile = arr[i];
+                bestGScore = arr[i].gScore;
+            }
+        }
+
+        return bestTile;
     }
 
     //Siege Phase
+
+    public Tile[] CalculatePath(Tile origin, Tile destination)
+    {
+        List<Tile> openList = new List<Tile>();
+        List<Tile> closedList = new List<Tile>();
+
+        openList.Add(origin);
+
+        while (openList.Count != 0 &&               // still stuff left to explore
+               !closedList.Contains(destination))   // AND we haven't reached the destination yet
+        {
+            // TODO: replace this with a proper sorted array implementation
+            Tile current = GetCheapestTile(openList.ToArray());
+            openList.Remove(current);
+
+            closedList.Add(current);
+
+            // TODO...
+            // calculate g scores for connected tiles
+            throw new System.NotImplementedException();
+        }
+
+        // TODO: remove this when completed
+        return null;
+    }
+
+    
 
 }
