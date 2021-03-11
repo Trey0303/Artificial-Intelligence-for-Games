c289ef30d4949f8399465179d90a746ea0c1fafd
Trey Gleason <treygleason8@gmail.com>
2021-03-10 17:49:21 -0800

    added enemy patrol and seek ai

--- Assets/Scenes/other/Scripts/FiniteStateMachines.cs
+++ Assets/Scenes/other/Scripts/FiniteStateMachines.cs
@@ -66,6 +66,8 @@ public class FiniteStateMachines : MonoBehaviour
         agent.velocity = (goalPos - curPos).normalized * speed;//gets the velocity of agent
         agent.UpdateMovement();//updates movement changes
 
+        agent.transform.forward = (goalPos - curPos).normalized;
+
         if((goalPos - agent.transform.position).magnitude < waypointReachedThreshold)//if goalPos - agent.transform.position is less than waypointReachedThreshold(1.0f)
         {
             ++currentWaypointIndex;
