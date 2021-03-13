4c2b12ef320fd870266e5c1cbd6f548ea3387ebe
Trey Gleason <treygleason8@gmail.com>
2021-03-12 17:12:28 -0800

    Made a build for my assessment and added a quit button

--- Assets/Scenes/Assessment/Scripts/RetryQuit.cs
+++ Assets/Scenes/Assessment/Scripts/RetryQuit.cs
@@ -12,4 +12,10 @@ public class RetryQuit : MonoBehaviour
         FiniteStateMachines.gameOver = false;
     }
 
+    //quit game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
